e33524f9de3462f86aa4b65c3bb8ea47d2671054
rabellan <roland.abellano@gmail.com>
2023-12-12 09:48:56 -0800

    Updated wash_frequency

--- static/js/wash_frequency.js
+++ static/js/wash_frequency.js
@@ -31,16 +31,16 @@ function buildGaugeChart(sample) {
             axis: {range: [0,10], tickmode: "linear", tick0: 2, dtick: 2},
             bar: {color: "green"},
             steps: [
-                {range: [0, 1], color: "rgba(255, 255, 255, 0)"},
-                {range: [1, 2], color: "rgba(232, 226, 202, .5)"},
-                {range: [2, 3], color: "rgba(210, 206, 145, .5)"},
-                {range: [3, 4], color:  "rgba(202, 209, 95, .5)"},
-                {range: [4, 5], color:  "rgba(184, 205, 68, .5)"},
-                {range: [5, 6], color: "rgba(170, 202, 42, .5)"},
-                {range: [6, 7], color: "rgba(142, 178, 35 , .5)"},
-                {range: [7, 8], color:  "rgba(110, 154, 22, .5)"},
-                {range: [8, 9], color: "rgba(50, 143, 10, 0.5)"},
                 {range: [9, 10], color: "rgba(14, 127, 0, .5)"},
+                {range: [8, 9], color: "rgba(50, 143, 10, 0.5)"},
+                {range: [7, 8], color: "rgba(110, 154, 22, .5)"},
+                {range: [6, 7], color: "rgba(142, 178, 35, .5)"},
+                {range: [5, 6], color: "rgba(170, 202, 42, .5)"},
+                {range: [4, 5], color: "rgba(184, 205, 68, .5)"},
+                {range: [3, 4], color: "rgba(202, 209, 95, .5)"},
+                {range: [2, 3], color: "rgba(210, 206, 145, .5)"},
+                {range: [1, 2], color: "rgba(232, 226, 202, .5)"},
+                {range: [0, 1], color: "rgba(255, 255, 255, 0)"}
             ]
         } 
     };
